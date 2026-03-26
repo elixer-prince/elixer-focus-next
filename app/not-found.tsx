ba5ce5555d3b7c6021ee2cfa6c861af7c3026574
Daniel Leith <elixerprince@gmail.com>
2026-03-26 17:53:12 -0500

    feat(app): create not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div>
+      <p>Not Found Page</p>
+
+      <Link href="/">Go Home</Link>
+    </div>
+  );
+};
+
+export default NotFound;
