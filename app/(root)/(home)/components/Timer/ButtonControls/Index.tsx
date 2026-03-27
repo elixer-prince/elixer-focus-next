d8d7ea5e9d21b8aad70baa8883f6239de20b8e85
Daniel Leith <elixerprince@gmail.com>
2026-03-27 10:29:38 -0500

    chore(ButtonControls): create button controls

---
+++ app/(root)/(home)/components/Timer/ButtonControls/Index.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import PauseButton from "@/app/(root)/(home)/components/Timer/ButtonControls/PauseButton";
+import ResetButton from "@/app/(root)/(home)/components/Timer/ButtonControls/ResetButton";
+import SkipButton from "@/app/(root)/(home)/components/Timer/ButtonControls/SkipButton";
+import StartButton from "@/app/(root)/(home)/components/Timer/ButtonControls/StartButton";
+import { useTimerPaused, useTimerRunning } from "@/app/stores/timer/countdown";
+
+const ButtonControls = () => {
+  const timerPaused = useTimerPaused();
+  const timerRunning = useTimerRunning();
+
+  return (
+    <div className={"mb-8 flex justify-center gap-4"}>
+      {timerPaused && <StartButton />}
+      {!timerPaused && <PauseButton />}
+      {timerRunning && <ResetButton />}
+      <SkipButton />
+    </div>
+  );
+};
+
+export default ButtonControls;
---
+++ app/(root)/(home)/components/Timer/ButtonControls/ResetButton.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import useResetCountdown from "@/app/hooks/timer/useResetCountdown";
+import { MdReplay } from "react-icons/md";
+
+const ResetButton = () => {
+  const { resetCountdownWithSound } = useResetCountdown();
+
+  return (
+    <button
+      className="btn btn-error btn-soft"
+      onClick={resetCountdownWithSound}
+    >
+      <MdReplay size={24} />
+      Reset
+    </button>
+  );
+};
+
+export default ResetButton;
