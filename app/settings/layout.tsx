17e31efc45ff8facdc6f87677e916ee9443a651d
Daniel Leith <elixerprince@gmail.com>
2026-03-27 20:08:58 -0500

    refactor(setting): remove unnecessary div wrapper and Link component from the layout.

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -1,16 +1,15 @@
 import Sidebar from "@/app/settings/_components/navigation/Sidebar/Index";
-import Link from "next/link";
 import { PropsWithChildren } from "react";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div>
+    <>
       <Sidebar />
 
       <main className="p-8 max-sm:mt-(--navbar-height) sm:ml-(--sidebar-width)">
         {children}
       </main>
-    </div>
+    </>
   );
 };
 
